f7767187e053ddfc1a17cd6a4668bff53af5ef4a
David Lam <David.Lam@DecisionResearch.com>
2016-11-18 15:36:38 -1000

    Completed bob, hello world

---
+++ csharp/bob/Bob.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Bob
+{
+    public static string Hey(string say)
+    {
+        string reply = string.Empty;
+        say = say.Trim();
+
+        if (say.Length == 0)
+        {
+            reply = "Fine. Be that way!";  //Saying nothing
+        }
+        else if (IsAllUpper(say))
+        {
+            reply = "Whoa, chill out!"; // Yell All Upper Case
+        }
+        else if (say.Substring(say.Length - 1, 1) == "?") // y.Substring(y.Length-1, 1);
+        {
+            reply = "Sure.";
+        }
+        else
+        {
+            reply = "Whatever.";
+        }
+
+        return reply;
+    }
+    
+    private static bool IsAllUpper(string input)
+    {
+        bool allUpper = true;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (Char.IsLetter(input[i]) && Char.IsLower(input[i]))
+                return false;
+        }
+
+        if (!input.Any(c => Char.IsUpper(c)) && !input.Any(c => Char.IsLower(c)))
+        {
+            allUpper = false;
+        }
+        return allUpper;
+    }
+}
---
+++ csharp/hello-world/HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.hello_world
+{
+    public class HelloWorld
+    {
+        public static string Hello(string msg)
+        {
+            string result = string.Empty;
+            if (msg == null)
+            {
+                result = "Hello, World!";
+            }
+            else {
+                result = $"Hello, {msg}!";
+            }
+            return result;
+        }
+
+    }
+}
