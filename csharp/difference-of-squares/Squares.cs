d3f762916e9295b27b0fdb395898024919dbc342
David Lam <David.Lam@DecisionResearch.com>
2016-11-21 11:25:28 -1000

    Completed Diference of Squares

---
+++ csharp/difference-of-squares/Squares.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Squares
+{
+    List<int> list;
+    
+
+    public Squares(int number)
+    {
+        if (number < 0) {
+            throw new ArgumentException();
+        }
+        list = Enumerable.Range(1,number).ToList();
+    }
+
+    public int SumOfSquares()
+    {
+        var result = list
+                         .Select(a => Math.Pow(a, 2))
+                         .Sum()
+                         ;
+
+        return (int) result;
+    }
+    public  int SquareOfSums() {
+        return (int)Math.Pow(list.Sum(), 2);
+
+    }
+    public  int DifferenceOfSquares()
+    {
+        return SquareOfSums() - SumOfSquares();
+    }
+}
